bcdccf82e3e384f5c098bd8731cb265941c71556
rmassey95 <rmassey.95@gmail.com>
2022-12-09 15:32:51 -0500

    Add general info section with ability to edit

--- src/App.js
+++ src/App.js
@@ -1,355 +1,359 @@
-import React, { Component } from "react";
+import React from "react";
 import GeneralInfo from "./components/GeneralInfo";
-import Education from "./components/Education";
-import Experience from "./components/Experience";
-import ProfDev from "./components/ProfDev";
 import "./styles/App.css";
-import uniqid from "uniqid";
 
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      generalInfo: {
-        name: "",
-        address: "",
-        phoneNum: "",
-        email: "",
-        edit: true,
-      },
-      education: {
-        title: "",
-        date: "",
-        school: "",
-        highlights: "",
-        add: false,
-        edit: false,
-        key: uniqid(),
-      },
-      educationArr: [],
-      experience: {
-        title: "",
-        date: "",
-        company: "",
-        highlights: "",
-        add: false,
-        edit: false,
-        key: uniqid(),
-      },
-      experienceArr: [],
-      profDev: {
-        highlights: "",
-        add: false,
-        edit: false,
-        key: uniqid(),
-      },
-      profDevArr: [],
-    };
-  }
-
-  update = (e, component, key = null) => {
-    e.preventDefault();
-
-    if (component === "generalInfo") {
-      this.setState({
-        generalInfo: {
-          name: e.target[0].value,
-          address: e.target[1].value,
-          phoneNum: e.target[2].value,
-          email: e.target[3].value,
-          edit: false,
-        },
-      });
-    } else if (component === "education") {
-      let eduUpdate = this.state.educationArr.map((edu) => {
-        if (edu.key === key) {
-          edu.title = e.target[0].value;
-          edu.date = e.target[1].value;
-          edu.school = e.target[2].value;
-          edu.highlights = e.target[3].value;
-          edu.add = false;
-          edu.edit = false;
-        }
-        return edu;
-      });
-
-      this.setState({
-        education: {
-          title: "",
-          date: "",
-          school: "",
-          highlights: "",
-          add: false,
-          edit: false,
-          key: uniqid(),
-        },
-        educationArr: eduUpdate,
-      });
-    } else if (component === "experience") {
-      let expUpdate = this.state.experienceArr.map((exp) => {
-        if (exp.key === key) {
-          exp.title = e.target[0].value;
-          exp.date = e.target[1].value;
-          exp.company = e.target[2].value;
-          exp.highlights = e.target[3].value;
-          exp.add = false;
-          exp.edit = false;
-        }
-        return exp;
-      });
-
-      this.setState({
-        experience: {
-          title: "",
-          date: "",
-          company: "",
-          highlights: "",
-          add: false,
-          edit: false,
-          key: uniqid(),
-        },
-        experienceArr: expUpdate,
-      });
-    } else if (component === "profDev") {
-      let profDevUpdate = this.state.profDevArr.map((profDev) => {
-        if (profDev.key === key) {
-          profDev.highlights = e.target[0].value;
-          profDev.add = false;
-          profDev.edit = false;
-        }
-        return profDev;
-      });
-
-      this.setState({
-        profDev: {
-          highlights: "",
-          add: false,
-          edit: false,
-          key: uniqid(),
-        },
-        profDevArr: profDevUpdate,
-      });
-    }
-  };
-
-  handleChange = (e, component) => {
-    if (component === "education") {
-      this.setState({
-        education: {
-          ...this.state.education,
-          [e.target.name]: e.target.value,
-        },
-      });
-    } else if (component === "experience") {
-      this.setState({
-        experience: {
-          ...this.state.experience,
-          [e.target.name]: e.target.value,
-        },
-      });
-    } else if (component === "profDev") {
-      this.setState({
-        profDev: {
-          ...this.state.profDev,
-          [e.target.name]: e.target.value,
-        },
-      });
-    }
-  };
-
-  add = (e, component) => {
-    e.preventDefault();
-
-    if (component === "education") {
-      let edu = this.state.education;
-
-      this.setState({
-        education: {
-          title: "",
-          date: "",
-          school: "",
-          highlights: "",
-          add: false,
-          edit: false,
-          key: uniqid(),
-        },
-        educationArr: this.state.educationArr.concat(edu),
-      });
-    } else if (component === "experience") {
-      let exp = this.state.experience;
-
-      this.setState({
-        experience: {
-          title: "",
-          date: "",
-          company: "",
-          highlights: "",
-          add: false,
-          edit: false,
-          key: uniqid(),
-        },
-        experienceArr: this.state.experienceArr.concat(exp),
-      });
-    } else if (component === "profDev") {
-      let profDev = this.state.profDev;
-
-      this.setState({
-        profDev: {
-          highlights: "",
-          add: false,
-          edit: false,
-          key: uniqid(),
-        },
-        profDevArr: this.state.profDevArr.concat(profDev),
-      });
-    }
-  };
-
-  setEdit = (component, key = null) => {
-    if (component === "generalInfo") {
-      this.setState({
-        generalInfo: {
-          ...this.state.generalInfo,
-          edit: true,
-        },
-      });
-    } else if (component === "education") {
-      let eduUpdate = this.state.educationArr.map((edu) => {
-        if (edu.key === key) {
-          edu.edit = true;
-        }
-        return edu;
-      });
-
-      this.setState({
-        educationArr: eduUpdate,
-      });
-    } else if (component === "experience") {
-      let expUpdate = this.state.experienceArr.map((exp) => {
-        if (exp.key === key) {
-          exp.edit = true;
-        }
-        return exp;
-      });
-
-      this.setState({
-        experienceArr: expUpdate,
-      });
-    } else if (component === "profDev") {
-      let profDevUpdate = this.state.profDevArr.map((profDev) => {
-        if (profDev.key === key) {
-          profDev.edit = true;
-        }
-        return profDev;
-      });
-
-      this.setState({
-        profDevArr: profDevUpdate,
-      });
-    }
-  };
-
-  setAdd = (component) => {
-    if (component === "education") {
-      this.setState({
-        education: {
-          ...this.state.education,
-          add: true,
-        },
-      });
-    } else if (component === "experience") {
-      this.setState({
-        experience: {
-          ...this.state.experience,
-          add: true,
-        },
-      });
-    } else if (component === "profDev") {
-      this.setState({
-        profDev: {
-          ...this.state.profDev,
-          add: true,
-        },
-      });
-    }
-  };
-
-  remove = (component, key) => {
-    if (component === "education") {
-      let eduUpdate = this.state.educationArr.filter((edu) => edu.key !== key);
-
-      this.setState({
-        educationArr: eduUpdate,
-      });
-    } else if (component === "experience") {
-      let expUpdate = this.state.experienceArr.filter((exp) => exp.key !== key);
-
-      this.setState({
-        experienceArr: expUpdate,
-      });
-    } else if (component === "profDev") {
-      let profDevUpdate = this.state.profDevArr.filter(
-        (profDev) => profDev.key !== key
-      );
-
-      this.setState({
-        profDevArr: profDevUpdate,
-      });
-    }
-  };
-
-  render() {
-    const {
-      generalInfo,
-      education,
-      educationArr,
-      experience,
-      experienceArr,
-      profDev,
-      profDevArr,
-    } = this.state;
-
-    return (
-      <div className="App">
-        <GeneralInfo
-          generalInfo={generalInfo}
-          update={this.update}
-          setEdit={this.setEdit}
-        />
-        <Education
-          education={education}
-          setEdit={this.setEdit}
-          educationArr={educationArr}
-          add={this.add}
-          handleChange={this.handleChange}
-          setAdd={this.setAdd}
-          update={this.update}
-          remove={this.remove}
-        />
-        <Experience
-          experience={experience}
-          experienceArr={experienceArr}
-          setEdit={this.setEdit}
-          add={this.add}
-          handleChange={this.handleChange}
-          setAdd={this.setAdd}
-          update={this.update}
-          remove={this.remove}
-        />
-        <ProfDev
-          profDev={profDev}
-          profDevArr={profDevArr}
-          setEdit={this.setEdit}
-          add={this.add}
-          handleChange={this.handleChange}
-          setAdd={this.setAdd}
-          update={this.update}
-          remove={this.remove}
-        />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <GeneralInfo />
+    </div>
+  );
 }
+
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       generalInfo: {
+//         name: "",
+//         address: "",
+//         phoneNum: "",
+//         email: "",
+//         edit: true,
+//       },
+//       education: {
+//         title: "",
+//         date: "",
+//         school: "",
+//         highlights: "",
+//         add: false,
+//         edit: false,
+//         key: uniqid(),
+//       },
+//       educationArr: [],
+//       experience: {
+//         title: "",
+//         date: "",
+//         company: "",
+//         highlights: "",
+//         add: false,
+//         edit: false,
+//         key: uniqid(),
+//       },
+//       experienceArr: [],
+//       profDev: {
+//         highlights: "",
+//         add: false,
+//         edit: false,
+//         key: uniqid(),
+//       },
+//       profDevArr: [],
+//     };
+//   }
+
+//   update = (e, component, key = null) => {
+//     e.preventDefault();
+
+//     if (component === "generalInfo") {
+//       this.setState({
+//         generalInfo: {
+//           name: e.target[0].value,
+//           address: e.target[1].value,
+//           phoneNum: e.target[2].value,
+//           email: e.target[3].value,
+//           edit: false,
+//         },
+//       });
+//     } else if (component === "education") {
+//       let eduUpdate = this.state.educationArr.map((edu) => {
+//         if (edu.key === key) {
+//           edu.title = e.target[0].value;
+//           edu.date = e.target[1].value;
+//           edu.school = e.target[2].value;
+//           edu.highlights = e.target[3].value;
+//           edu.add = false;
+//           edu.edit = false;
+//         }
+//         return edu;
+//       });
+
+//       this.setState({
+//         education: {
+//           title: "",
+//           date: "",
+//           school: "",
+//           highlights: "",
+//           add: false,
+//           edit: false,
+//           key: uniqid(),
+//         },
+//         educationArr: eduUpdate,
+//       });
+//     } else if (component === "experience") {
+//       let expUpdate = this.state.experienceArr.map((exp) => {
+//         if (exp.key === key) {
+//           exp.title = e.target[0].value;
+//           exp.date = e.target[1].value;
+//           exp.company = e.target[2].value;
+//           exp.highlights = e.target[3].value;
+//           exp.add = false;
+//           exp.edit = false;
+//         }
+//         return exp;
+//       });
+
+//       this.setState({
+//         experience: {
+//           title: "",
+//           date: "",
+//           company: "",
+//           highlights: "",
+//           add: false,
+//           edit: false,
+//           key: uniqid(),
+//         },
+//         experienceArr: expUpdate,
+//       });
+//     } else if (component === "profDev") {
+//       let profDevUpdate = this.state.profDevArr.map((profDev) => {
+//         if (profDev.key === key) {
+//           profDev.highlights = e.target[0].value;
+//           profDev.add = false;
+//           profDev.edit = false;
+//         }
+//         return profDev;
+//       });
+
+//       this.setState({
+//         profDev: {
+//           highlights: "",
+//           add: false,
+//           edit: false,
+//           key: uniqid(),
+//         },
+//         profDevArr: profDevUpdate,
+//       });
+//     }
+//   };
+
+//   handleChange = (e, component) => {
+//     if (component === "education") {
+//       this.setState({
+//         education: {
+//           ...this.state.education,
+//           [e.target.name]: e.target.value,
+//         },
+//       });
+//     } else if (component === "experience") {
+//       this.setState({
+//         experience: {
+//           ...this.state.experience,
+//           [e.target.name]: e.target.value,
+//         },
+//       });
+//     } else if (component === "profDev") {
+//       this.setState({
+//         profDev: {
+//           ...this.state.profDev,
+//           [e.target.name]: e.target.value,
+//         },
+//       });
+//     }
+//   };
+
+//   add = (e, component) => {
+//     e.preventDefault();
+
+//     if (component === "education") {
+//       let edu = this.state.education;
+
+//       this.setState({
+//         education: {
+//           title: "",
+//           date: "",
+//           school: "",
+//           highlights: "",
+//           add: false,
+//           edit: false,
+//           key: uniqid(),
+//         },
+//         educationArr: this.state.educationArr.concat(edu),
+//       });
+//     } else if (component === "experience") {
+//       let exp = this.state.experience;
+
+//       this.setState({
+//         experience: {
+//           title: "",
+//           date: "",
+//           company: "",
+//           highlights: "",
+//           add: false,
+//           edit: false,
+//           key: uniqid(),
+//         },
+//         experienceArr: this.state.experienceArr.concat(exp),
+//       });
+//     } else if (component === "profDev") {
+//       let profDev = this.state.profDev;
+
+//       this.setState({
+//         profDev: {
+//           highlights: "",
+//           add: false,
+//           edit: false,
+//           key: uniqid(),
+//         },
+//         profDevArr: this.state.profDevArr.concat(profDev),
+//       });
+//     }
+//   };
+
+//   setEdit = (component, key = null) => {
+//     if (component === "generalInfo") {
+//       this.setState({
+//         generalInfo: {
+//           ...this.state.generalInfo,
+//           edit: true,
+//         },
+//       });
+//     } else if (component === "education") {
+//       let eduUpdate = this.state.educationArr.map((edu) => {
+//         if (edu.key === key) {
+//           edu.edit = true;
+//         }
+//         return edu;
+//       });
+
+//       this.setState({
+//         educationArr: eduUpdate,
+//       });
+//     } else if (component === "experience") {
+//       let expUpdate = this.state.experienceArr.map((exp) => {
+//         if (exp.key === key) {
+//           exp.edit = true;
+//         }
+//         return exp;
+//       });
+
+//       this.setState({
+//         experienceArr: expUpdate,
+//       });
+//     } else if (component === "profDev") {
+//       let profDevUpdate = this.state.profDevArr.map((profDev) => {
+//         if (profDev.key === key) {
+//           profDev.edit = true;
+//         }
+//         return profDev;
+//       });
+
+//       this.setState({
+//         profDevArr: profDevUpdate,
+//       });
+//     }
+//   };
+
+//   setAdd = (component) => {
+//     if (component === "education") {
+//       this.setState({
+//         education: {
+//           ...this.state.education,
+//           add: true,
+//         },
+//       });
+//     } else if (component === "experience") {
+//       this.setState({
+//         experience: {
+//           ...this.state.experience,
+//           add: true,
+//         },
+//       });
+//     } else if (component === "profDev") {
+//       this.setState({
+//         profDev: {
+//           ...this.state.profDev,
+//           add: true,
+//         },
+//       });
+//     }
+//   };
+
+//   remove = (component, key) => {
+//     if (component === "education") {
+//       let eduUpdate = this.state.educationArr.filter((edu) => edu.key !== key);
+
+//       this.setState({
+//         educationArr: eduUpdate,
+//       });
+//     } else if (component === "experience") {
+//       let expUpdate = this.state.experienceArr.filter((exp) => exp.key !== key);
+
+//       this.setState({
+//         experienceArr: expUpdate,
+//       });
+//     } else if (component === "profDev") {
+//       let profDevUpdate = this.state.profDevArr.filter(
+//         (profDev) => profDev.key !== key
+//       );
+
+//       this.setState({
+//         profDevArr: profDevUpdate,
+//       });
+//     }
+//   };
+
+//   render() {
+//     const {
+//       generalInfo,
+//       education,
+//       educationArr,
+//       experience,
+//       experienceArr,
+//       profDev,
+//       profDevArr,
+//     } = this.state;
+
+//     return (
+//       <div className="App">
+//         <GeneralInfo
+//           generalInfo={generalInfo}
+//           update={this.update}
+//           setEdit={this.setEdit}
+//         />
+//         <Education
+//           education={education}
+//           setEdit={this.setEdit}
+//           educationArr={educationArr}
+//           add={this.add}
+//           handleChange={this.handleChange}
+//           setAdd={this.setAdd}
+//           update={this.update}
+//           remove={this.remove}
+//         />
+//         <Experience
+//           experience={experience}
+//           experienceArr={experienceArr}
+//           setEdit={this.setEdit}
+//           add={this.add}
+//           handleChange={this.handleChange}
+//           setAdd={this.setAdd}
+//           update={this.update}
+//           remove={this.remove}
+//         />
+//         <ProfDev
+//           profDev={profDev}
+//           profDevArr={profDevArr}
+//           setEdit={this.setEdit}
+//           add={this.add}
+//           handleChange={this.handleChange}
+//           setAdd={this.setAdd}
+//           update={this.update}
+//           remove={this.remove}
+//         />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,88 +1,97 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "../styles/GeneralInfo.css";
 
-class GeneralInfo extends Component {
-  constructor(props) {
-    super();
+function GeneralInfo() {
+  const [generalInfo, setGeneralInfo] = useState({
+    name: "",
+    address: "",
+    phoneNum: "",
+    email: "",
+    edit: true,
+  });
+
+  function update(e) {
+    e.preventDefault();
+    console.log("PREVENTED DEFAULT");
+    setGeneralInfo({ ...generalInfo, edit: false });
   }
 
-  displayForm = (generalInfo, update) => {
+  function handleChange(e) {
+    setGeneralInfo({ ...generalInfo, [e.target.name]: e.target.value });
+  }
+
+  function setEdit() {
+    setGeneralInfo({ ...generalInfo, edit: true });
+  }
+
+  console.log(generalInfo);
+
+  if (generalInfo.edit) {
+    //display form
     return (
       <form
-        className="general-info"
         onSubmit={(e) => {
-          update(e, "generalInfo");
+          update(e);
         }}
       >
-        <input
-          className="general-info-input"
-          placeholder="Full name"
-          defaultValue={generalInfo.name}
-          required
-          type="text"
-        ></input>
-        <input
-          className="general-info-input"
-          placeholder="Address"
-          defaultValue={generalInfo.address}
-          required
-          type="text"
-        ></input>
-        <input
-          className="general-info-input"
-          placeholder="Phone Number"
-          defaultValue={generalInfo.phoneNum}
-          required
-          type="tel"
-        ></input>
-        <input
-          className="general-info-input"
-          placeholder="Email"
-          defaultValue={generalInfo.email}
-          required
-          type="email"
-        ></input>
-
+        <div className="input">
+          <label htmlFor="name">Full Name: </label>
+          <input
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            value={generalInfo.name}
+            id="name"
+            name="name"
+          ></input>
+        </div>
+        <div className="input">
+          <label htmlFor="address">Address: </label>
+          <input
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            value={generalInfo.address}
+            id="address"
+            name="address"
+          ></input>
+        </div>
+        <div className="input">
+          <label htmlFor="phoneNum">Phone Number: </label>
+          <input
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            value={generalInfo.phoneNum}
+            id="phoneNum"
+            name="phoneNum"
+          ></input>
+        </div>
+        <div className="input">
+          <label htmlFor="email">Email: </label>
+          <input
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            value={generalInfo.email}
+            id="email"
+            name="email"
+          ></input>
+        </div>
         <button>Submit</button>
       </form>
     );
-  };
-
-  displayOutput = (generalInfo, setEdit) => {
-    return (
-      <div className="general-info">
-        <h1>{generalInfo.name}</h1>
-        <p>{generalInfo.address}</p>
-        <p>
-          {generalInfo.phoneNum} {generalInfo.email}
-        </p>
-        <button
-          className="edit-btn"
-          onClick={() => {
-            setEdit("generalInfo");
-          }}
-        >
-          Edit
-        </button>
-      </div>
-    );
-  };
-
-  render() {
-    const { generalInfo, update, setEdit } = this.props;
-
-    return (
-      <div>
-        {(() => {
-          if (generalInfo.edit) {
-            return this.displayForm(generalInfo, update);
-          } else {
-            return this.displayOutput(generalInfo, setEdit);
-          }
-        })()}
-      </div>
-    );
   }
+  //display info
+  return (
+    <div>
+      <p>{generalInfo.name}</p>
+      <p>{generalInfo.address}</p>
+      <p>{generalInfo.phoneNum}</p>
+      <p>{generalInfo.email}</p>
+      <button onClick={setEdit}>Edit</button>
+    </div>
+  );
 }
 
 export default GeneralInfo;
